4d89809c37c91379bfe1cc48c3957e39f0c562c2
aezarebski <aezarebski@gmail.com>
2022-12-16 12:47:37 +0000

    docs

--- demo-c.R
+++ demo-c.R
@@ -8,6 +8,12 @@ epi <- simulate_epidemic(uid, params, config)
 epi_summary <- summarise_epidemic(uid, epi)
 print(epi_summary)
 epi_msa <- simulate_genomes(epi, params)
+## Save a copy of the sequences so that they can be used in a BDSKY analysis.
+phangorn::write.phyDat(
+            epi_msa,
+            config$files$simulation$fastafile(uid),
+            format = "fasta"
+          )
 mcmc_xml <- get_beast_mcmc_xml(uid, epi_msa, NULL, params, config)
 xml2::write_xml(mcmc_xml, "out/configuration-c/demo-c-mcmc.xml")
 mcmc_samples <- run_beast_mcmc(mcmc_xml, config)
