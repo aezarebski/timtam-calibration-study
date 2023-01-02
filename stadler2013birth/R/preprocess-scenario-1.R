befa718de56a5f830497366e4f0bec409872d615
Alex Zarebski <aezarebski@gmail.com>
2023-01-02 17:16:58 +0000

    start on a bdsky xml

---
+++ stadler2013birth/R/preprocess-scenario-1.R
@@ -0,0 +1,63 @@
+suppressPackageStartupMessages(library(ape))
+suppressPackageStartupMessages(library(phangorn))
+suppressPackageStartupMessages(library(dplyr))
+suppressPackageStartupMessages(library(ggplot2))
+suppressPackageStartupMessages(library(purrr))
+suppressPackageStartupMessages(library(magrittr))
+suppressPackageStartupMessages(library(stringr))
+suppressPackageStartupMessages(library(xml2))
+set.seed(1)
+
+
+make_record <- function(x) {
+  x |>
+    as.numeric() |>
+    as.list() |>
+    set_names(c("t", "Psi", "X", "Mu")) |>
+    as.data.frame()
+}
+
+## TODO Fix this so that it can generate the simulation data for each sample
+## properly.
+
+sim_data <- "remaster-scenario-1.log" |>
+  readLines() |>
+  pluck(2) |>
+  str_split("\t") |>
+  unlist() |>
+  pluck(2) |>
+  str_split(";") |>
+  unlist() |>
+  str_replace_all("(t|Psi|X|Mu)=", "") |>
+  str_split(":") |>
+  map(make_record) |>
+  bind_rows()
+
+## TODO Fix this so it will extract the trees and write the results to a
+## sensible output file.
+
+recon_tree <- "remaster-scenario-1.tree" |> read.nexus() |> pluck(1)
+tmp_tip_labs <- recon_tree$tip.label
+fwd_times_rel <- recon_tree |>
+  node.depth.edgelength() |>
+  head(length(tmp_tip_labs))
+
+ix <- nrow(sim_data)
+while (ix > 1) {
+  if (sim_data[ix, "Psi"] > sim_data[ix - 1, "Psi"]) {
+    last_seq_time <- sim_data[ix, "t"]
+    break
+  }
+  ix <- ix - 1
+}
+rm(ix)
+
+fwd_times <- last_seq_time + fwd_times_rel - max(fwd_times_rel)
+recon_tree$tip.label <- sprintf("%s_%f", tmp_tip_labs, fwd_times)
+
+seqs <- simSeq(
+  recon_tree,
+  l = 1,
+  rate = .Machine$double.eps
+)
+write.phyDat(seqs, "demo-sequences-scenario-1-sample-1.fasta", format = "fasta")
