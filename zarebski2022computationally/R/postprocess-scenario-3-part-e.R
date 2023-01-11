cc8fff9f63ab49925db21932f65b13354ad14ab9
Alex Zarebski <aezarebski@gmail.com>
2023-01-11 13:57:47 +0000

    include posterior summary under time series approximation

---
+++ zarebski2022computationally/R/postprocess-scenario-3-part-e.R
@@ -0,0 +1,212 @@
+suppressPackageStartupMessages(library(ape))
+suppressPackageStartupMessages(library(phangorn))
+suppressPackageStartupMessages(library(dplyr))
+suppressPackageStartupMessages(library(ggplot2))
+suppressPackageStartupMessages(library(purrr))
+suppressPackageStartupMessages(library(magrittr))
+suppressPackageStartupMessages(library(stringr))
+suppressPackageStartupMessages(library(xml2))
+suppressPackageStartupMessages(library(coda))
+suppressPackageStartupMessages(library(reshape2))
+library(xtable)
+set.seed(1)
+
+#' Read a BEAST2 log file into a data frame.
+#'
+#' @param filename is the path to the log file.
+#' @param burn is the number to remove from the start.
+#' @param take_last is the number to take from the end.
+#'
+#' @return data frame containing the samples.
+#'
+read_beast2_log <- function(filename, burn = 0, take_last = NA) {
+  y <- read.csv(filename, sep = "\t", comment.char = "#")
+  if (is.na(take_last) && burn >= 0) {
+    return(tail(y, nrow(y) - burn))
+  } else if (!is.na(take_last) && burn == 0) {
+    return(tail(y, take_last))
+  } else {
+    stop("Unsupported arguments given to read_beast2_log.")
+  }
+}
+
+remaster_node <- "xml/remaster-scenario-3.xml" |> read_xml()
+num_replicates <- remaster_node |>
+  xml_find_first("//run") |>
+  xml_attr("nSims") |>
+  as.integer()
+
+log_files <-
+  list.files("out/s3", pattern = "*.log", full.names = TRUE) |>
+  str_subset("3-3")
+
+if (length(log_files) != num_replicates) {
+  stop("Number of log files does not match number of replicates.")
+}
+
+final_prev_df <- read.csv("out/s3/final-simulation-state.csv") |>
+  mutate(replicate = sample + 1) |>
+  select(X, replicate)
+
+in_ci <- function(x, posts) {
+  ci <- quantile(posts, probs = c(0.025, 0.975))
+  ci[1] <= x && x <= ci[2]
+}
+ci_width <- function(posts) {
+  ci <- quantile(posts, probs = c(0.025, 0.975))
+  ci[2] - ci[1]
+}
+bias <- function(x, posts) {
+  median((posts - x) / x)
+}
+error <- function(x, posts) {
+  median(abs((posts - x) / x))
+}
+log_file_summary <- function(log_file, params) {
+  replicate_num <- log_file |>
+    str_extract("[0-9]{3}") |>
+    as.integer()
+  final_prev <- final_prev_df |>
+    filter(replicate == replicate_num) |>
+    pluck("X")
+  with(read_beast2_log(log_file), {
+    ApproxOmegaRate <-
+      2 * TTNuProb / (2 * params$ts_interval - TTNuProb * params$ts_interval)
+    net_removal_rate <- params$dr + TTSamplingRate + ApproxOmegaRate
+    TTREff1 <- TTBirthRate.1 / net_removal_rate
+    TTREff2 <- TTBirthRate.2 / net_removal_rate
+    true_reff_1 <- params$br1 / (params$dr + params$sr + params$or)
+    true_reff_2 <- params$br2 / (params$dr + params$sr + params$or)
+    data.frame(
+      m_birth_rate_1 = median(TTBirthRate.1),
+      m_birth_rate_2 = median(TTBirthRate.2),
+      m_sampling_rate = median(TTSamplingRate),
+      m_occurrence_rate = median(ApproxOmegaRate),
+      m_reff_1 = median(TTREff1),
+      m_reff_2 = median(TTREff2),
+      b_birth_rate_1 = bias(params$br1, TTBirthRate.1),
+      b_birth_rate_2 = bias(params$br2, TTBirthRate.2),
+      b_sampling_rate = bias(params$sr, TTSamplingRate),
+      b_occurrence_rate = bias(params$or, ApproxOmegaRate),
+      b_reff_1 = bias(true_reff_1, TTREff1),
+      b_reff_2 = bias(true_reff_2, TTREff2),
+      ## b_prev = bias(final_prev, HistorySizes),
+      e_birth_rate_1 = error(params$br1, TTBirthRate.1),
+      e_birth_rate_2 = error(params$br2, TTBirthRate.2),
+      e_sampling_rate = error(params$sr, TTSamplingRate),
+      e_occurrence_rate = error(params$or, ApproxOmegaRate),
+      e_reff_1 = error(true_reff_1, TTREff1),
+      e_reff_2 = error(true_reff_2, TTREff2),
+      ## e_prev = error(final_prev, HistorySizes),
+      w_birth_rate_1 = ci_width(TTBirthRate.1),
+      w_birth_rate_2 = ci_width(TTBirthRate.2),
+      w_sampling_rate = ci_width(TTSamplingRate),
+      w_occurrence_rate = ci_width(ApproxOmegaRate),
+      w_reff_1 = ci_width(TTREff1),
+      w_reff_2 = ci_width(TTREff2),
+      in_ci_birth_rate_1 = in_ci(params$br1, TTBirthRate.1),
+      in_ci_birth_rate_2 = in_ci(params$br2, TTBirthRate.2),
+      in_ci_sampling_rate = in_ci(params$sr, TTSamplingRate),
+      in_ci_occurrence_rate = in_ci(params$or, ApproxOmegaRate),
+      in_ci_reff_1 = in_ci(true_reff_1, TTREff1),
+      in_ci_reff_2 = in_ci(true_reff_2, TTREff2),
+      ## in_ci_prev = in_ci(final_prev, HistorySizes),
+      log_file = log_file
+    )
+  })
+}
+
+params <- list(
+  br1 = 0.185,
+  br2 = 0.0925,
+  dr = 0.046,
+  sr = 0.008,
+  or = 0.046,
+  hs = 0,
+  ts_interval = 1.0
+)
+replicate_summaries <- map(log_files, \(lf) log_file_summary(lf, params)) |>
+  bind_rows()
+
+result <- with(replicate_summaries, {
+  percent_true <- function(bs) {
+    100 * sum(bs) / length(bs)
+  }
+  data.frame(
+    parameter = c(
+      "birth rate 1", "birth rate 2",
+      "sampling rate", "approximate occurrence rate",
+      "R effective 1", "R effective 2"
+      ## "prevalence"
+    ),
+    true = with(params, c(br1, br2, sr, or, br1/(dr+sr+or), br2/(dr+sr+or))),
+    median = c(
+      median(m_birth_rate_1), median(m_birth_rate_2),
+      median(m_sampling_rate), median(m_occurrence_rate),
+      median(m_reff_1), median(m_reff_2)
+      ## NA
+    ),
+    error = c(
+      median(e_birth_rate_1), median(e_birth_rate_2),
+      median(e_sampling_rate), median(e_occurrence_rate),
+      median(e_reff_1), median(e_reff_2)
+      ## median(e_prev)
+    ),
+    bias = c(
+      median(b_birth_rate_1), median(b_birth_rate_2),
+      median(b_sampling_rate), median(b_occurrence_rate),
+      median(b_reff_1), median(b_reff_2)
+      ## median(b_prev)
+    ),
+    ci_width = c(
+      median(w_birth_rate_1), median(w_birth_rate_2),
+      median(w_sampling_rate), median(w_occurrence_rate),
+      median(w_reff_1), median(w_reff_2)
+      ## NA
+    ),
+    ci_percent = c(
+      percent_true(in_ci_birth_rate_1), percent_true(in_ci_birth_rate_2),
+      percent_true(in_ci_sampling_rate), percent_true(in_ci_occurrence_rate),
+      percent_true(in_ci_reff_1), percent_true(in_ci_reff_2)
+      ## percent_true(in_ci_prev)
+    )
+  )
+})
+row.names(result) <- NULL
+
+result <- xtable(result)
+align(result) <- xalign(result)
+digits(result) <- xdigits(result, zap = 3)
+display(result) <- xdisplay(result)
+print(result, include.rownames = FALSE)
+
+## % latex table generated in R 4.2.1 by xtable 1.8-4 package
+
+
+#' Hypothesis test on the binomial probability.
+#'
+#' @param x_obs integer number of successes observed.
+#' @param size integer number of tests carried out.
+#' @param prob probability of success in each test
+#'
+calibration_test <- function(x_obs, size, prob = 0.95) {
+  pmf_vals <- dbinom(x = 0:size, prob = prob, size = size)
+  obs_prob <- dbinom(x = x_obs, prob = prob, size = size)
+  mask <- pmf_vals <= obs_prob
+  p_val <- sum(pmf_vals[mask])
+  list(
+    x = x_obs,
+    n = size,
+    p_val = p_val,
+    reject_null = p_val < 0.05
+  )
+}
+
+list(
+  as.data.frame(calibration_test(90, 100)),
+  as.data.frame(calibration_test(91, 100)),
+  as.data.frame(calibration_test(99, 100)),
+  as.data.frame(calibration_test(100, 100))
+) |>
+  bind_rows() |>
+  print()
