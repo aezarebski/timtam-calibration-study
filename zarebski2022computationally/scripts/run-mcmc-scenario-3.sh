cd1691688a5f8205bbc6cd9cb0d5e37bca2ec14b
Alex Zarebski <azarebski@6200l-214217-l.mobility.unimelb.net.au>
2023-04-21 15:12:53 +1000

    pass more arguments through to the mcmc call

--- zarebski2022computationally/scripts/run-mcmc-scenario-3.sh
+++ zarebski2022computationally/scripts/run-mcmc-scenario-3.sh
@@ -22,15 +22,17 @@ ANALYSIS_NUM=$2
 
 CHAIN_LEN=$3
 
+echo "Running $NUM_REPLICATES each of length $CHAIN_LEN"
+
 while [ $IX -le $OUTER_LIMIT ]
 do
     PIDS=()
     CHUNK_LIMIT=$((IX+CHUNK_SIZE))
     while [ $IX -lt $CHUNK_LIMIT ]
     do
-	      PADDED_NUM=$(printf "%03d" $IX) # because we want zero padded numbers
-        ant -DchainLength=$CHAIN_LEN -DbeastXML=out/s3/timtam-scenario-3-"$ANALYSIS_NUM"-sample-"$PADDED_NUM".xml mcmc & PIDS+=($!)
-	      ((IX++))
+	PADDED_NUM=$(printf "%03d" $IX) # because we want zero padded numbers
+        ant -DstateFile=out/tmp/timtam-scenario-3-"$ANALYSIS_NUM"-sample-"$PADDED_NUM".xml.state -DchainLength=$CHAIN_LEN -DbeastXML=out/s3/timtam-scenario-3-"$ANALYSIS_NUM"-sample-"$PADDED_NUM".xml mcmc & PIDS+=($!)
+	((IX++))
     done
     wait "${PIDS[@]}"
     echo "---"
