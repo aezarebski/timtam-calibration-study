e3eeea00b3afdee4a9a1aed885e9228ba1506d9b
aezarebski <aezarebski@gmail.com>
2024-03-21 10:29:08 +1100

    add a docstring for read_mcmc

--- R/helper-functions.R
+++ R/helper-functions.R
@@ -2,6 +2,13 @@ suppressPackageStartupMessages(library(dplyr))
 suppressPackageStartupMessages(library(coda))
 
 
+#' Read MCMC log file
+#'
+#' Read MCMC log file and return an MCMC object
+#'
+#' @param log_file Path to the log file
+#' @return MCMC object
+#'
 read_mcmc <- function(log_file) {
   if (!file.exists(log_file)) {
     stop("File does not exist: ", log_file)
